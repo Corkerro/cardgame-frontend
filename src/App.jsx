79e644ab75aabf88e46e432b0d77f9f6e512078d
Corkerro <74060229+Corkerro@users.noreply.github.com>
2025-05-21 21:57:02 +0300

    add auth check and loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 // App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import AuthPage from './pages/AuthPage/AuthPage';
 import LobbyPage from './pages/LobbyPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './assets/styles/reset.css';
 import './assets/styles/base.scss';
-import GamePage from "./pages/GamePage.jsx";
+import GamePage from './pages/GamePage.jsx';
+import RequireAuth from './components/RequireAuth.jsx';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/auth" element={<AuthPage />} />
-                <Route path="/" element={<LobbyPage />} />
-                <Route path="/game" element={<GamePage />} />
+
+                <Route element={<RequireAuth />}>
+                    <Route path="/" element={<LobbyPage />} />
+                    <Route path="/game" element={<GamePage />} />
+                </Route>
+
                 {/* other routes */}
+
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
             <ToastContainer
                 position="bottom-right"
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import '../assets/styles/loader.scss'
+
+export default function Loader() {
+    return (
+        <div className="loader">
+            <div className="loader__spinner" />
+        </div>
+    );
+}
