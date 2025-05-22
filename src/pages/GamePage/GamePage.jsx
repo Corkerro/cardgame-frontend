02a7df259bb1bf1758292ef98a0a723823124b19
Corkerro <74060229+Corkerro@users.noreply.github.com>
2025-05-22 16:32:57 +0300

    add card

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../assets/styles/game/gamepage.scss';
+import '../../assets/styles/game/card.scss';
 import Player from './Player.jsx';
 
 export default function GamePage({ onNavigate }) {
@@ -24,6 +25,28 @@ export default function GamePage({ onNavigate }) {
                 <img src="logo.png" alt="" className="gamepage__logo" />
                 <Player player={player} />
             </aside>
+            <div className="gamebox">
+                <div className="card">
+                    <ul className="card__stats">
+                        <li className="card__stat">
+                            <img src="sword.svg" alt="Attac" />
+                            <span>10</span>
+                        </li>
+                        <li className="card__stat">
+                            <img src="shield.svg" alt="Defense" />
+                            <span>8</span>
+                        </li>
+                        <li className="card__stat">
+                            <img src="money.svg" alt="Price" />
+                            <span>10</span>
+                        </li>
+                    </ul>
+
+                    <img src="cards/card-1.png" alt="Card Name" className="card__img" />
+
+                    <p className="card__name">Card Name</p>
+                </div>
+            </div>
         </div>
     );
 }
